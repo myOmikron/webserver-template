4320645cbbbe3d0cbc90588078966729f2e74e03
myOmikron <git@omikron.dev>
2024-05-31 12:16:43 +0200

    Added react-toastify

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,13 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./router";
 import { LoggingSwitch } from "./utils/console";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <LoggingSwitch />
+        <ToastContainer />
         <RouterProvider router={router} />
     </React.StrictMode>,
 );
